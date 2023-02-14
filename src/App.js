d625dabbbb905a774f62df3d875744c4bcdb0810
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-14 17:27:36 +0200

    create elem hw

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
-import React from 'react';
+
 
 function App() {
+    const h1 = React.createElement('h1', {}, 'unit_08')
+    const h2 = React.createElement('h2', {
+        'className': 'text-orange',
+    }, 'header 2')
+    const p = React.createElement('p', {
+        'style': {
+            'color': 'red'
+        }
+    }, 'this is p')
+    const input = React.createElement('input', {
+        'defaultValue': '55'
+    })
+    const p1 = React.createElement('p', {}, 'hi');
+    const p2 = React.createElement('p', {}, 'world');
+    const div = React.createElement('div', {
+        'className': 'text-grey'
+    }, p1, p2);
 
-  const title = React.createElement('h1', {}, 'Text header 1');
+    const val = React.createRef();
+    const [out, setOut] = useState();
 
-  return (
-    <>
-      <React.Fragment>
-        Hello
-      </React.Fragment>
-      
-    </>
-  );
+    function value() {
+        let val_1 = val.current.value;
+        
+        setOut(val_1)
+        val.current.value = '';
+        if (val_1 !== '') return setOut(val_1); 
+    }
+
+    console.log(out)
+    return (
+        <>
+            {h1}
+            {h2}
+            {p}
+            {div}
+            {input}
+            <div>
+            <input type="text" ref={val}/>
+                <button onClick={value}>Push</button>
+            </div>
+            <ul>
+                {out}
+                </ul>
+        </>
+    )
 }
 
 export default App;
---
+++ src/App1.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+
+  const cE = React.createElement;
+  const title = React.createElement('h1', {}, 'Text header 1');
+  const title2 = React.createElement('h2', {
+    'color': 'red', 'style': {
+      'color': 'red',
+      'background': 'orange',
+
+    },
+    'id': 'hide',
+    'className': 'italic bold'
+  }, 'Text header 2');
+
+  const s = cE('span', {}, 'this is a span');
+  const example = cE('p', {}, s);
+
+  return (
+    <>
+      <React.Fragment>
+        Hello
+      </React.Fragment>
+      {title}
+      <div>
+        {title2}
+      </div>
+      <div>
+        {cE('input', {})}
+      </div>
+      <div>
+        {example}
+      </div>
+    </>
+  );
+}
+
+export default App;
